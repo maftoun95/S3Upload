be8be79ea31127ec45723a080f777d9b6304e324
Saman M <maftoun95@gmail.com>
2017-07-15 17:10:38 -0700

    initializing repository

---
+++ index.js
@@ -0,0 +1,10 @@
+var refresh = document.querySelector('.refresh');
+var download = document.querySelector('.download');
+
+refresh.addEventListener('click', function(){
+    alert("refreshing");
+});
+
+download.addEventListener('click', function(){
+    alert("downloading");
+});
